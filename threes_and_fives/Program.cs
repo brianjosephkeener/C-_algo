dacf4da3785ccc0e0866cf45b56c07b9f3564cc3
brian120496 <brian120496@gmail.com>
2022-02-03 17:16:55 -0500

    add three_and_fives

---
+++ threes_and_fives/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace threes_and_fives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
