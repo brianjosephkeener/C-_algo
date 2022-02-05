f1e8679c5b55e1898363667040bf6a049b56f937
Brian Keener <brian120496@gmail.com>
2022-02-05 01:35:18 -0500

    fibonacci/clock/last digit algos

---
+++ Clock_Hand_Angles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+
+Traditional clocks are increasingly uncommon, but most can still read rotating hands of hours, minutes,
+and seconds.
+Create function clockHandAngles(seconds)​ that, given the number of seconds since 12:00:00, will
+print the angles (in degrees) of the hour, minute and second hands. As a quick review, there are 360
+degrees in a full arc rotation. Treat “straight-up” 12:00:00 as 0 degrees for all hands.
+
+*/
+
+namespace Clock_Hand_Angles
+{
+    class Program
+    {
+        
+        public static void clockHandAngles(double seconds)
+        {
+            double hourh = 0; double minuteh = 0; double secondh = 0;
+            hourh = (seconds * (0.00833333333));
+            while(hourh >= 360)
+            {
+                hourh = hourh - 360;
+            }
+            minuteh = (seconds * (0.1));
+            while(minuteh >= 360)
+            {
+                minuteh = minuteh - 360;
+            }
+            secondh = (seconds * 6);
+            while(secondh >= 360)
+            {
+                secondh = secondh - 360;
+            }
+            Console.WriteLine($"hour hand degrees: {hourh}, minute hand degrees: {minuteh}, second hand degrees: {secondh}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            clockHandAngles(20000);
+        }
+    }
+}
---
+++ Last_Digit_of_A_to_the_B/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+
+Last Digit of A to the B
+Modern computers can handle very large numbers, but this ability has limits. If a number is repeatedly
+multiplied by itself, it eventually exceeds the computer’s ability to accurately represent it. (Side note: the
+number of times it is multiplied by itself is called the exponent.) For an optional end-of-chapter
+challenge, determine the smallest (least significant) digit of a number that is potentially very, very large.
+You may find that you must do this without computing the actual (unimaginably large) number.
+Implement a function that accepts two non-negative integers as arguments. Function lastDigitAtoB(a,
+b)​ should return the last digit of the first number (a) raised to an exponent of the second number (b).
+Examples: given (3, 4)​, you should return 1​ (the last digit of 81: 3 * 3 * 3 * 3). Given (12, 5)​, return
+2​ (the least significant digit of 248832, which is 12 * 12 * 12 * 12 * 12).
+
+*/
+
+namespace Last_Digit_of_A_to_the_B
+{
+    class Program
+    {
+    static int Modulo(int a, char[] b)
+    {
+        int mod = 0;
+        for (int i = 0; i < b.Length; i++)
+            mod = (mod * 10 + b[i] - '0') % a;
+        return mod;
+    }
+    static int LastDigit(char[] a, char[] b)
+    {
+        int len_a = a.Length, len_b = b.Length;
+        if (len_a == 1 && len_b == 1 &&
+                   b[0] == '0' && a[0] == '0')
+            return 1;
+        if (len_b == 1 && b[0] == '0')
+            return 1;
+        if (len_a == 1 && a[0] == '0')
+            return 0;
+        int exp = (Modulo(4, b) == 0) ? 4 : Modulo(4, b);
+        int res = (int)(Math.Pow(a[len_a - 1] - '0', exp));
+        return res % 10;
+    }
+        static void Main(string[] args)
+        {
+        char[] a = "117".ToCharArray(),
+        b = { '3' };
+        Console.Write(LastDigit(a, b));
+        }
+    }
+}
