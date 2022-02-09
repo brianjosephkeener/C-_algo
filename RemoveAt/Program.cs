f3649782852695344ca5f24a144a57a202a8b8fc
Brian Keener <brian120496@gmail.com>
2022-02-09 11:08:41 -0500

    RemoveAtv1

---
+++ RemoveAt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoveAt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
