225fe1a84e9efcb93a19fa21039d5f52b24b11ee
Brian Keener <brian120496@gmail.com>
2022-02-09 11:15:06 -0500

    removeatv2

--- RemoveAt/Program.cs
+++ RemoveAt/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+
+RemoveAt
+
+Given an array and an index into the array,
+remove and return the array value at that index.
+
+*/
 
 namespace RemoveAt
 {
@@ -6,7 +16,18 @@ namespace RemoveAt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> array = new List<string>
+                {
+                    "hello",
+                    "world",
+                    "create",
+                    "program"
+                };
+            array.RemoveAt(0);
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
