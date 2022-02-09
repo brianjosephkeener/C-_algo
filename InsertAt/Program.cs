05758f4c5bacaa6eaa1146c5ab47001e53f0c8f8
Brian Keener <brian120496@gmail.com>
2022-02-09 11:07:05 -0500

    organize folder / InsertAt

---
+++ InsertAt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+
+InsertAt
+
+Given an array, index, and additional value, insert
+the value into the array at the given index. 
+
+
+*/
+
+namespace InsertAt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Array = new List<int>{
+            100,
+            5,
+            1500,
+            93,
+            1
+            };
+            Array.Insert(2, 1234);
+            foreach (int item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
