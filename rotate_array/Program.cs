832261f59500324a4a52729c85267a8c4310be7f
Skull86 <brian120496@gmail.com>
2022-02-10 03:46:39 -0500

    rotate array v1

---
+++ rotate_array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+
+Rotate Array
+
+Implement ​rotateArr(arr, shiftBy)​ that accepts array and offset. Shift arr’s values ​to the right​ by that amount. 
+‘Wrap-around’ any values that shift off array’s end to the other side, so that no data is lost. 
+Operate in-place: given ([1,2,3],1)​, change the array to [​ 3,1,2]​.
+
+*/
+
+namespace rotate_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
