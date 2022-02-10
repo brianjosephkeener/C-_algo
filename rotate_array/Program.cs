e78603159d20c83576e68acf49f7aea477c44692
Skull86 <brian120496@gmail.com>
2022-02-10 04:08:53 -0500

    rotate_arrayv2

--- rotate_array/Program.cs
+++ rotate_array/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 Rotate Array
 
-Implement ​rotateArr(arr, shiftBy)​ that accepts array and offset. Shift arr’s values ​to the right​ by that amount. 
+Implement ​rotateArr (arr, shiftBy)​ that accepts array and offset. Shift arr’s values ​to the right​ by that amount. 
 ‘Wrap-around’ any values that shift off array’s end to the other side, so that no data is lost. 
 Operate in-place: given ([1,2,3],1)​, change the array to [​ 3,1,2]​.
 
@@ -15,9 +15,31 @@ namespace rotate_array
 {
     class Program
     {
+        static void RArray(List<int> arr, int ramount)
+        {
+            List<int> rarr = new List<int>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if(ramount > i)
+                {
+                    rarr.Add(arr[arr.Count-ramount+i]);
+                }
+                else
+                {
+                    rarr.Add(arr[i-ramount]);
+                }
+            }
+            foreach (int item in rarr)
+            {
+                Console.WriteLine(item);
+            }
+        }
         static void Main(string[] args)
         {
-            
+            List<int> testArr = new List<int>{
+                1, 2, 3
+            };
+            RArray(testArr, 1);
         }
     }
 }
