8718ae8b5c3b38410c26762c0907dec0c9fbcae5
Skull86 <brian120496@gmail.com>
2022-02-11 03:05:39 -0500

    nth-to_end

---
+++ nth-to-last/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nth_to_last
+{
+
+    /*
+
+    Nth-to-Last
+    Return the element that is N-from-array’s-end.  
+
+    */
+
+    class Program
+    {
+        static int nAway(int[] arr, int aend)
+        {
+            return (arr[arr.Length - aend]);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 5, 10, 18, 3, -2, 555, 1234, 5976};
+            Console.WriteLine(nAway(arr, 4));
+        }
+    }
+}
