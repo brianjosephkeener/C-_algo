1a9bb000594cd611d5c5ff581e2bacf7f8e5faea
Skull86 <brian120496@gmail.com>
2022-02-11 03:32:53 -0500

    nth-largest-array

--- nth-largest-in-array/Program.cs
+++ nth-largest-in-array/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
+
+/*
+
+Nth-Largest
+
+Given an array, return the Nth-largest element: there should be (N - 1) elements that are larger.
+
+*/
 
 namespace nth_largest_in_array
 {
     class Program
     {
+        static int NthElement(int[] arr, int nth)
+        {
+            var sortedArray = arr.OrderBy(num => num);
+            var QS = (from num in arr
+                      orderby num
+                      select num).ToArray();
+            Console.WriteLine(QS[QS.Length - nth]);
+            return QS[QS.Length - nth];
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] testArray = {1, 5, 3, 100, 500, 5, 2, 18, 11};
+            NthElement(testArray, 3);
         }
     }
 }
