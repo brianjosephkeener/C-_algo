a5d8b49964be5c48c5c2bde75e1a9f23de90b694
Skull86 <brian120496@gmail.com>
2022-02-10 21:55:41 -0500

    2nd_to_last

---
+++ 2nd_to_last_in_array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+
+Second-to-Last
+
+Return the second-to-last element of an array.
+
+*/
+
+namespace _2nd_to_last_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testArray = {"hello", "world", "name", "is", "brian"};
+            Console.WriteLine(testArray[testArray.Length-2]);
+        }
+    }
+}
