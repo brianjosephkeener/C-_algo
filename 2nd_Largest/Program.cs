6d791f1f9ae04de83f39fdd246b2bb277a0aa097
Skull86 <brian120496@gmail.com>
2022-02-10 22:29:50 -0500

    2nd_largest_in_array

---
+++ 2nd_Largest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+/*
+
+Second-Largest
+
+Return the second-largest element of an array.
+
+*/
+
+namespace _2nd_Largest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = {1, 5, 3, 100, 500, 5, 2, 18, 11};
+            var sortedArray = testArray.OrderBy(num => num);
+
+            var QS = (from num in testArray
+                      orderby num
+                      select num).ToArray();
+            Console.WriteLine(QS[QS.Length - 2]);
+        }
+    }
+}
