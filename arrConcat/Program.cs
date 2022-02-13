14e1eb541f3c7552ea2971cdb56610cbbae73d79
Skull86 <brian120496@gmail.com>
2022-02-12 22:38:39 -0500

    concat

--- arrConcat/Program.cs
+++ arrConcat/Program.cs
@@ -1,4 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+
+arrConcat
+
+Replicate JavaScript’s ​concat()​. 
+Return a ​new​ array containing the first array’s elements, followed by the second array’s elements. 
+Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] )​ should return ​['a','b',1,2]​.
+
+*/
 
 namespace arrConcat
 {
@@ -6,7 +18,11 @@ namespace arrConcat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        List<int> first = new List<int>() { 1, 2, 3, 4, 5 };
+        List<int> second = new List<int>() { 4, 7, 8, 9 };
+
+        List<int> result = first.Concat(second).ToList();
+        Console.WriteLine(String.Join(",", result));
         }
     }
 }
