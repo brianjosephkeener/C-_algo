929506a166c5d6e6abb95dd8e7a4f13e11f26b59
Skull86 <brian120496@gmail.com>
2022-02-12 22:42:41 -0500

    shuffle array

---
+++ Shuffle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Random random = new Random();
+            arr = arr.OrderBy(x => random.Next()).ToArray();
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
