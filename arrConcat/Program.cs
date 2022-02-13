a7d881a7b05149dcf37d9c5c210e2632b9180f86
Skull86 <brian120496@gmail.com>
2022-02-12 22:31:17 -0500

    new algo

---
+++ arrConcat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
