a97e67ddd80926dbb89463a95899870e559dae4c
Skull86 <brian120496@gmail.com>
2022-02-14 03:53:11 -0500

    final version

--- parentheses_valid/Program.cs
+++ parentheses_valid/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 
+/*
+
+Parens Valid
+Create a function that, given an input string, returns a boolean whether parentheses in that string are valid. 
+Given input ​"y(3(p)p(3)r)s"​, return true. 
+Given ​"n(0(p)3"​, return ​false​. 
+Given ​"n)0(t(0)k"​, return ​false​.
+
+*/
+
 namespace parentheses_valid
 {
     class Program
     {
+        static bool ParVal(string str)
+        {
+            short count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(Char.ToString(str[i]) == ")")
+                {
+                    count--;
+                }
+                if(count < 0)
+                {
+                    return false;
+                }
+                if(Char.ToString(str[i]) == "(")
+                {
+                    count++;
+                }
+            }
+            if(count == 0)
+            {
+            return true;
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ParVal("y(3(p)p(3)r)s"));
+            Console.WriteLine(ParVal("n(0(p)3"));
+            Console.WriteLine(ParVal("n)0(t(0)k"));
         }
     }
 }
