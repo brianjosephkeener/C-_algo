d065e52ef1e7dfc938af8602c8227197b030f94f
Skull86 <brian120496@gmail.com>
2022-02-14 02:18:00 -0500

    invert hash

---
+++ InvertHash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvertHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
