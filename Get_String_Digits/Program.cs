9f58192ea3061f1a729a2619cf3db59c0bc9dbc5
Skull86 <brian120496@gmail.com>
2022-02-14 03:20:27 -0500

    get string digits

--- Get_String_Digits/Program.cs
+++ Get_String_Digits/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
+/*
+
+Get String Digits
+
+Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 01357924680.
+
+*/
+
 namespace Get_String_Digits
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test = "0s1a3y5w7h9a2t4?6!8?0";
+            for (int i = 0; i < test.Length; i++)
+            {
+                if(!Char.IsDigit(test[i]))
+                {
+                    test = test.Remove(i, 1);
+                }
+            }
+            Console.WriteLine(test);
         }
     }
 }
