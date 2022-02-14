23a0d2b29ab358ed4dcc21b7e33e7b45f2689d06
Skull86 <brian120496@gmail.com>
2022-02-14 03:41:55 -0500

    parentheses_validv1

---
+++ parentheses_valid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace parentheses_valid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
