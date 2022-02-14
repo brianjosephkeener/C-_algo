72af23d3648deefe5208f0545a5c55581f60a85a
Skull86 <brian120496@gmail.com>
2022-02-14 03:17:12 -0500

    get string digits

---
+++ Get_String_Digits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Get_String_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
