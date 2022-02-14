dfcfbb311d185a1d7ed68fce40ebc633c836fc2d
Skull86 <brian120496@gmail.com>
2022-02-14 03:15:54 -0500

    iswhitespace finished

--- remove Blanks/Program.cs
+++ remove Blanks/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
+/*
+
+Remove Blanks
+Create a function that, given a string, returns the string, without blanks. 
+Given ​" play that Funky Music "​, returns a string containing ​"playthatFunkyMusic"​.
+
+*/
+
 namespace remove_Blanks
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test = "123 456 789";
+            for (int i = 0; i < test.Length; i++)
+            {
+                if(Char.IsWhiteSpace(test[i]))
+                {
+                    test = test.Remove(i, 1);
+                }
+            }
+            Console.WriteLine(test);
         }
     }
 }
