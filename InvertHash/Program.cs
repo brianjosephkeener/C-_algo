8ca19932c755d41b5dd9333f6abd71d9ecbffec4
Skull86 <brian120496@gmail.com>
2022-02-14 02:34:56 -0500

    invert hash finished

--- InvertHash/Program.cs
+++ InvertHash/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+
+Create invertHash(assocArr) that converts a hash’s keys to values and values to corresponding keys. 
+Example: given ​{"name": "Zaphod"; "numHeads": 2}​, return ​{"Zaphod": "name"; 2: "numHeads"}​.
+
+*/
 
 namespace InvertHash
 {
     class Program
     {
+        static Dictionary<object, object> invertHash(Dictionary<object, object> hasharr)
+        {
+            Dictionary<object, object> dicinvert = new Dictionary<object, object>();
+            foreach (var item in hasharr)
+            {
+                dicinvert.Add(item.Value, item.Key);
+            }
+            return dicinvert;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<object, object> dic = new Dictionary<object, object>{
+                {"Brian Keener", "Developer"},
+                {"hash key", 123},
+                {456, "value"}
+            };
+            foreach (var item in invertHash(dic))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
