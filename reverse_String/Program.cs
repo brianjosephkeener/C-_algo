de45bbded9b809717869bbcdd29a030563d3a4bd
Skull86 <brian120496@gmail.com>
2022-02-14 02:50:23 -0500

    reversestring finished

--- reverse_String/Program.cs
+++ reverse_String/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+/*
+
+ReverseString
+Implement a function reverseString(str) that, given a string, will return the string of the same length but with characters reversed. 
+Example: given ​"creature"​, return ​"erutaerc"​. Do not use the built-in reverse()​ function!
+
+*/
+
 namespace reverse_String
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test = "test123";
+            string reversetest = "";
+            for (int i = 0; i < test.Length; i++)
+            {
+                reversetest += (test[test.Length-1-i]);
+            }
+            Console.WriteLine(reversetest);
         }
     }
 }
