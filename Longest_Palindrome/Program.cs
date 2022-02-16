b0308140f098e58708371db99bb29470cc0dd644
Skull86 <brian120496@gmail.com>
2022-02-16 14:34:32 -0500

    longest pal

---
+++ Longest_Palindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Longest_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
