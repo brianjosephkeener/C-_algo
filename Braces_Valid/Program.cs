873335f884e2acea370e7f098a2753a6fcda3b81
Skull86 <brian120496@gmail.com>
2022-02-15 20:39:06 -0500

    v2?

---
+++ Braces_Valid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Braces_Valid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
