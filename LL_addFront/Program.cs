8d40e80881311cc26aacc33ab5f8d03c7902e01f
Skull86 <brian120496@gmail.com>
2022-02-17 14:18:06 -0500

    LinkedList Front

--- LL_addFront/Program.cs
+++ LL_addFront/Program.cs
@@ -4,9 +4,56 @@ namespace LL_addFront
 {
     class Program
     {
+        class LinkedListNode
+        {
+            public int data;
+            public LinkedListNode next;
+            public LinkedListNode(int x)
+            {
+                data = x;
+                next = null;
+            }
+        }
+
+        class LinkedList
+        {
+            int count;
+            LinkedListNode head;
+
+            public LinkedList()
+            {
+                head = null;
+                count = 0;
+            }
+
+            public void AddNodeToFront(int data)
+            {
+                LinkedListNode node = new LinkedListNode(data);
+                node.next = head;
+                head = node;
+                count++;
+            }
+        public void PrintList()
+            {
+                LinkedListNode runner = head;
+                while(runner != null)
+                {
+                    Console.WriteLine(runner.data);
+                    runner = runner.next;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddNodeToFront(5);
+            linkedList.AddNodeToFront(13);
+            linkedList.AddNodeToFront(45);
+            linkedList.AddNodeToFront(91);
+            linkedList.AddNodeToFront(91);
+            linkedList.AddNodeToFront(11);
+            linkedList.PrintList();
         }
     }
 }
