80aecf1e1ace064731a768506afae3007b49509e
Skull86 <brian120496@gmail.com>
2022-02-17 13:31:47 -0500

    linkedlist add to front

---
+++ LL_addFront/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LL_addFront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
