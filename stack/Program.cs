f6ebfc518af85d83a350076a6092386d1b5f3455
Skull86 <brian120496@gmail.com>
2022-02-25 18:23:51 -0500

    stack finished

--- stack/Program.cs
+++ stack/Program.cs
@@ -10,14 +10,21 @@ namespace stack
             // return top value of stack without removing it
             return s.ToArray()[0];
         }
+        static object IsEmpty(Stack s)
+        {
+            return (s.Count < 1) ? true : false;
+        }
         static void Main(string[] args)
         {
             Stack s = new Stack();
+            Console.WriteLine(IsEmpty(s));
+
             // adding values to stack with push
             s.Push(1);
             s.Push(2);
             s.Push(3);
             s.Push(4);
+            Console.WriteLine(IsEmpty(s));
             // removing top value to stack with pop
             s.Pop();
             foreach (var item in s)
@@ -25,7 +32,7 @@ namespace stack
                 Console.WriteLine(item);
             }
             Console.WriteLine(Top(s));
-            
+            // check if stack has certain values
             Console.WriteLine(s.Contains(3));
             Console.WriteLine(s.Contains(4));
         }
