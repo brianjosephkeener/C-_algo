230e487542b11dac01cf2f18b29856079e6965f9
Skull86 <brian120496@gmail.com>
2022-02-26 13:22:12 -0500

    start

--- Balance_Point/Program.cs
+++ Balance_Point/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
+/*
+
+Balance Point
+
+Write a function that returns whether the given array has a balance point between indices, where one side’s sum is equal to the other’s. 
+Example: ​[1,2,3,4,10]​ → ​true​ (​between indices 3 & 4)​ , but [1,2,4,2,1]​ → ​false​.
+
+*/ 
+
 namespace Balance_Point
 {
     class Program
     {
+        static bool bPoint(int[] arr)
+        {
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr1 = new int[] {1,2,3,4,10};
+            int[] arr2 = new int[] {1,2,4,2,1};
+
         }
     }
 }
