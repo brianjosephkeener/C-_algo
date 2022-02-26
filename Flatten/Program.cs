29bd95b1908b0f01e224f8356abdebdb38b16d65
Skull86 <brian120496@gmail.com>
2022-02-26 13:47:34 -0500

    flatten

--- Flatten/Program.cs
+++ Flatten/Program.cs
@@ -2,11 +2,40 @@
 
 namespace Flatten
 {
-    class Program
+
+class Program
+{
+    static int[] To1DArray(int[,] input)
     {
-        static void Main(string[] args)
+        // Step 1: get total size of 2D array, and allocate 1D array.
+        int size = input.Length;
+        int[] result = new int[size];
+
+        // Step 2: copy 2D array elements into a 1D array.
+        int write = 0;
+        for (int i = 0; i <= input.GetUpperBound(0); i++)
         {
-            Console.WriteLine("Hello World!");
+            for (int z = 0; z <= input.GetUpperBound(1); z++)
+            {
+                result[write++] = input[i, z];
+            }
+        }
+        // Step 3: return the new array.
+        return result;
+    }
+
+    static void Main()
+    {
+        int[,] elements = { { 10, 20, 30 }, { 40, 50, 60 } };
+
+        // Convert 2D array to 1D array.
+        int[] result = To1DArray(elements);
+
+        // Write flat array.
+        foreach (int value in result)
+        {
+            Console.WriteLine("ELEMENT: {0}", value);
         }
     }
+}
 }
