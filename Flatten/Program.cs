2db085cbc351160223ae28a971d2c7ed75caf723
Skull86 <brian120496@gmail.com>
2022-02-26 13:38:57 -0500

    flatten

---
+++ Flatten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Flatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
