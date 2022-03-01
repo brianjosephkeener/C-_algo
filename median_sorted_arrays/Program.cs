a078e0c00e9a3e20e58b38eddca29ec6984d53de
Skull86 <brian120496@gmail.com>
2022-03-01 17:18:31 -0500

    median of sorted array any length

--- median_sorted_arrays/Program.cs
+++ median_sorted_arrays/Program.cs
@@ -16,15 +16,34 @@ namespace median_sorted_arrays
 {
     class Program
     {
+
         static int medianSortedArrays(List<List<int>> arr)
         {
-            return 0;
+            List<int> result = new List<int>();
+            foreach (List<int> itemTop in arr)
+            {
+                for (int i = 0; i < itemTop.Count; i++)
+                {
+                    result.Add(itemTop[i]);
+                }
+            }
+            result.Sort();
+            if(result.Count%2!=0)
+            {
+                return result[result.Count / 2];
+            }
+            else
+            {
+                return (result[(result.Count - 1) / 2] + result[result.Count / 2]) / 2;
+            }
         }
+
         static void Main(string[] args)
         {
             List<int> testArr = new List<int> {1, 5, 9};
             List<int> testArr2 = new List<int> {1, 2, 3, 4, 5, 6};
             List<List<int>> list = new List<List<int>> {testArr, testArr2};
+            Console.WriteLine(medianSortedArrays(list));
         }
     }
 }
