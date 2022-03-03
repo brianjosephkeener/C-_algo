077322f3475596f437bda522b6b25418bfda38d4
Skull86 <brian120496@gmail.com>
2022-03-03 17:24:53 -0500

    rSigma recursive

---
+++ rSigma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rSigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
