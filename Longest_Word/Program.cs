1de11390f1dc6b57c4ecd9bcd095ea46a12256c9
Skull86 <brian120496@gmail.com>
2022-03-06 18:47:01 -0500

    finished longest word

--- Longest_Word/Program.cs
+++ Longest_Word/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+
+Longest Word
+
+Create a function that, given a string of words, returns the longest word. 
+Example: given ​"Snap crackle pop makes the world go round!"​, return ​"crackle"​.
+
+*/
 
 namespace Longest_Word
 {
     class Program
     {
+        private static string lWord(string input)
+        {
+            string lword = "";
+            string temp = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(Char.IsWhiteSpace(input[i]) == false)
+                {
+                    temp+=input[i];
+                }
+                else if(i == input.Length - 1)
+                {
+                    if(temp.Length > lword.Length)
+                    {
+                        lword = temp;
+                    }
+                    temp = "";
+                }
+                else {
+                    if(temp.Length > lword.Length)
+                    {
+                        lword = temp;
+                    }
+                    temp = "";
+                }
+            }
+            return lword;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(lWord("Snap crackle pop makes the world go round!"));
         }
     }
 }
