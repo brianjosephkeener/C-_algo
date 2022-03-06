a1b441f728c273e5c2b18fb511f500b1f4fafeec
Skull86 <brian120496@gmail.com>
2022-03-05 23:50:29 -0500

    done

--- String2WordArray/Program.cs
+++ String2WordArray/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+
+String2WordArray
+
+Given a string of words (with spaces or tabs), returns an array of words. 
+Given "Life is not a drill!"​ return ​["Life", "is" "not", "a", "drill!"]​.
+
+
+*/
 
 namespace String2WordArray
 {
     class Program
     {
+        static List<string> Str2WArr(string input)
+        {
+            char[] inputArr = input.ToCharArray();
+            List<string> result = new List<string>();
+            string temp = "";
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                if(Char.IsWhiteSpace(inputArr[i]) != true)
+                {
+                    temp+=inputArr[i].ToString();
+                }
+                else {
+                    result.Add(temp);
+                    temp = "";
+                }
+                if(i == inputArr.Length - 1)
+                {
+                    result.Add(temp);
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (string item in Str2WArr("Life is not a drill!"))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
