0ba1b675393b5b0c9aa20d4e0c979ddf361a7aeb
Skull86 <brian120496@gmail.com>
2022-03-08 11:58:29 -0500

    rotate string

--- Rotate_String/Program.cs
+++ Rotate_String/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
+/*
+
+
+Rotate String
+Create a standalone function that accepts a string and an integer, and rotates the characters in the string to the right by that amount.
+Example: given ​("Boris Godunov",5)​, return ​"dunovBoris Go"​.
+
+*/
+
 namespace Rotate_String
 {
     class Program
     {
+        static string rString(string input, int amount)
+        {
+            string result = "";
+            for (int i = input.Length - amount; i < input.Length; i++)
+            {
+                result+=input[i];
+            }
+            for (int i = 0; i < input.Length - amount; i++)
+            {
+                result+=input[i];
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(rString("Boris Godunov", 5));
         }
     }
 }
