d24db13a233e9ea440371e21530e37f905a81876
Skull86 <brian120496@gmail.com>
2022-03-15 19:37:37 -0400

    encode done

--- String_Encode/Program.cs
+++ String_Encode/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
+/*
+
+String Encode
+
+You are given a string that may contain sequences of consecutive characters. 
+Create a function to shorten a string by including the character, then the number of times it appears. 
+For ​"aaaabbcddd"​, return ​"a4b2c1d3"​. If result is not shorter (such as ​"bb"​=>​"b2"​), return the original string.
+
+*/
+
 namespace String_Encode
 {
     class Program
     {
+        static string Encode(string input)
+        {
+            char[] inputarr = input.ToCharArray();
+            string result = "";
+            int count = 1;
+            for (int i = 0; i < inputarr.Length; i++)
+            {
+                    if(i+1 < inputarr.Length)
+                    {
+                        if(inputarr[i] == inputarr[i+1])
+                        {
+                            count++;
+                        }
+                        else 
+                        {
+                        result += inputarr[i].ToString() + count;
+                        count = 1;
+                        }
+                    }
+                    else 
+                    {
+                        result += inputarr[i].ToString() + count;
+                        count = 1;
+                    }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Encode("aaaabbcddd")); // returns a4b2c1d3
         }
     }
 }
