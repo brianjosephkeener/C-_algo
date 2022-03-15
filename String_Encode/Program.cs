bc577f5add9081511d7edc2795747768f05a7fb9
Skull86 <brian120496@gmail.com>
2022-03-15 19:16:55 -0400

    String Encode

---
+++ String_Encode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_Encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
