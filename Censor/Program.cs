c04ebcb23288656e0d332601c38303120a1d97c3
Skull86 <brian120496@gmail.com>
2022-03-15 01:06:18 -0400

    draft v3 (close)

--- Censor/Program.cs
+++ Censor/Program.cs
@@ -15,21 +15,40 @@ namespace Censor
         static string Censor(string inputStr, string[] nauWord)
         {
             string[] input = inputStr.Split(' ');
+            string result = "";
             for (int i = 0; i < input.Length; i++)
             {
+                char[] inputChArr = input[i].ToCharArray();
                 for (int j = 0; j < nauWord.Length; j++)
                 {
-                    int index = input[i].IndexOf(nauWord[j]);
-                    if(index > -1 )
+                    char[] nauWordChArr = nauWord[j].ToCharArray();
+                    int count = 0;
+                    for (int k = 0; k < inputChArr.Length; k++)
                     {
-                        for (int k = index; k < input[i].Length; k++)
+                        if(inputChArr[k] == nauWordChArr[count])
                         {
-                            
+                            count++;
+                            if(count == nauWordChArr.Length)
+                            {
+                                for (int l = Math.Abs(count-k-1); l < count; l++)
+                                {
+                                    Console.WriteLine(count);
+                                    Console.WriteLine(k);
+                                    inputChArr[l] = 'x';
+                                }
+                                count = 0;
+                            }
+                        }
+                        else
+                        {
+                            count = 0;
                         }
                     }
                 }
+            string convertedToString = new string(inputChArr);
+            result += convertedToString + " ";
             }
-            return "";
+        return result;
         }
         static void Main(string[] args)
         {
