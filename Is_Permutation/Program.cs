f8cb81913fb68b00164fd90bb59955b754de15dd
Skull86 <brian120496@gmail.com>
2022-03-15 18:49:31 -0400

    new algo

---
+++ Is_Permutation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Is_Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
