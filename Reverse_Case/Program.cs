494ae42dee091046f5c460a39e0a73f85dfd8951
brianjosephkeener <brianjosephkeener@gmail.com>
2022-06-29 11:25:22 -0400

    reverse_case

---
+++ Reverse_Case/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+ 
+
+/*
+
+ 
+
+Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+ 
+
+ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+ 
+
+ReverseCase("MANY THANKS") ➞ "many thanks"
+
+ 
+
+ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+
+ 
+
+*/
+
+                                                                               
+
+public class Program
+
+{
+
+                public static string ReverseCase(string input)
+
+                {
+
+                                string result = "";
+
+                                for(int i = 0; i < input.Length; i++ )
+
+                                {
+
+                                                var ternary = Char.IsUpper(input[i]) ?
+
+                                                                result += Char.ToLower(input[i])
+
+                                                                                      :
+
+                                                                result += Char.ToUpper(input[i]);
+
+                                }
+
+                                return result;
+
+                }
+
+                public static void Main()
+
+                {
+
+                                Console.WriteLine(ReverseCase("Happy Birthday"));
+
+                                Console.WriteLine(ReverseCase("MANY THANKS"));
+
+                                Console.WriteLine(ReverseCase("sPoNtAnEoUs"));
+
+                }
+
+}
